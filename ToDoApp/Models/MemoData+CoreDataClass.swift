2cbcfac3af0a2bca6f2ea5ecc6d8fb5d9a13b18d
jungseok-corine <nonehour@naver.com>
2023-12-22 20:26:16 +0900

    config: model

---
+++ ToDoApp/Models/MemoData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  MemoData+CoreDataClass.swift
+//  ToDoApp
+//
+//  Created by 오정석 on 22/12/2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(MemoData)
+public class MemoData: NSManagedObject {
+
+}
---
+++ ToDoApp/Models/MemoData+CoreDataProperties.swift
@@ -0,0 +1,27 @@
+//
+//  MemoData+CoreDataProperties.swift
+//  ToDoApp
+//
+//  Created by 오정석 on 22/12/2023.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension MemoData {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<MemoData> {
+        return NSFetchRequest<MemoData>(entityName: "MemoData")
+    }
+
+    @NSManaged public var memoText: String?
+    @NSManaged public var date: Date?
+    @NSManaged public var color: Int64
+
+}
+
+extension MemoData : Identifiable {
+
+}
